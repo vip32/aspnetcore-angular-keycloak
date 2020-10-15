ca547c4ec76830a126d138e941549ccbb3fa8178
vip32 <mini32@gmail.com>
2020-10-15 17:37:25 +0200

    request fix

--- src/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ src/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,8 +9,8 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    console.log('weather: get=', baseUrl + 'weatherforecast');
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => { // TODO: use generated API client
+    console.log('weather: get=', baseUrl + 'api/v1/weatherforecasts');
+    http.get<WeatherForecast[]>(baseUrl + 'api/v1/weatherforecasts').subscribe(result => { // TODO: use generated API client
       console.log('weather: data=', result);
       this.forecasts = result;
     }, error => console.error(error));
--- src/WebApp/ClientApp/src/app/fetch-profile/fetch-profile.component.ts
+++ src/WebApp/ClientApp/src/app/fetch-profile/fetch-profile.component.ts
@@ -9,8 +9,8 @@ export class FetchProfileComponent {
   public content: object;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    console.log('profile: get=', baseUrl + 'userprofile');
-    http.get<object>(baseUrl + 'userprofile').subscribe(result => { // TODO: use generated API client
+    console.log('profile: get=', baseUrl + 'api/v1/userprofile');
+    http.get<object>(baseUrl + 'api/v1/userprofile').subscribe(result => { // TODO: use generated API client
       console.log('profile: data=', result);
       this.content = result;
     }, error => console.error(error));
