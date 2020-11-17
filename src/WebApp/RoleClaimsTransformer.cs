a74690f06463cfca25fe397e2d09f35fb57ad589
vp <vp>
2020-11-17 22:28:08 +0100

    role claims

--- src/WebApp/RoleClaimsTransformer.cs
+++ src/WebApp/RoleClaimsTransformer.cs
@@ -1,12 +1,10 @@
 ﻿namespace WebApp
 {
-    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Authentication;
+    using Newtonsoft.Json.Linq;
     using System.Linq;
     using System.Security.Claims;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Authentication;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
 
     public class RoleClaimsTransformer : IClaimsTransformation
     {
@@ -14,61 +12,38 @@
         {
             ClaimsIdentity claimsIdentity = (ClaimsIdentity)principal.Identity;
 
-            // flatten realm_access element because Microsoft identity model doesn't support nested claims
+            // flatten elements because Microsoft identity model doesn't support nested claims
             // by map it to Microsoft identity model, because automatic JWT bearer token mapping already processed here
-            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "realm_access"))
+            TransformRoles(claimsIdentity, "realm_access");
+            TransformRoles(claimsIdentity, "resource_access");
+
+            return Task.FromResult(principal);
+        }
+
+        private static void TransformRoles(ClaimsIdentity claimsIdentity, string claimType)
+        {
+            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((c) => c.Type.Equals(claimType, System.StringComparison.OrdinalIgnoreCase)))
             {
-                var claim = claimsIdentity.FindFirst((claim) => claim.Type == "realm_access");
-                var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(claim.Value);
-                if (dictionary["roles"] != null)
+                var claim = claimsIdentity.FindFirst((c) => c.Type.Equals(claimType, System.StringComparison.OrdinalIgnoreCase));
+                if (claim == null || string.IsNullOrEmpty(claim.Value))
                 {
-                    foreach (var role in dictionary["roles"])
-                    {
-                        claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
-                    }
+                    return;
                 }
-            }
 
-            if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "resource_access"))
-            {
-                var claim = claimsIdentity.FindFirst((claim) => claim.Type.Equals("resource_access", System.StringComparison.OrdinalIgnoreCase));
-
-                if (claim != null && !string.IsNullOrEmpty(claim.Value))
+                foreach (var roles in
+                    JObject.Parse(claim.Value).SelectTokens("$..roles"))
                 {
-                    foreach (var roles in
-                        JObject.Parse(claim.Value).SelectTokens("$..roles"))
+                    foreach (var role in roles.Values<string>())
                     {
-                        if (roles?.Any() == true)
+                        if (!string.IsNullOrEmpty(role)
+                            && !claimsIdentity.Claims.Any(c =>
+                                c.Type.Equals(ClaimTypes.Role, System.StringComparison.OrdinalIgnoreCase) && c.Value.Equals(role, System.StringComparison.OrdinalIgnoreCase)))
                         {
-                            foreach (var role in roles.Values<string>())
-                            {
-                                if (!string.IsNullOrEmpty(role)
-                                    && !claimsIdentity.Claims.Any(c => c.Value.Equals(role, System.StringComparison.OrdinalIgnoreCase)))
-                                {
-                                    claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
-                                }
-                            }
+                            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
                         }
                     }
                 }
             }
-
-            // flatten roles element because Microsoft identity model doesn't support nested claims
-            // by map it to Microsoft identity model, because automatic JWT bearer token mapping already processed here
-            //if (claimsIdentity.IsAuthenticated && claimsIdentity.HasClaim((claim) => claim.Type == "roles"))
-            //{
-            //    var claim = claimsIdentity.FindFirst((claim) => claim.Type == "roles");
-            //    var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(claim.Value);
-            //    if (dictionary != null)
-            //    {
-            //        foreach (var role in dictionary)
-            //        {
-            //            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.Value));
-            //        }
-            //    }
-            //}
-
-            return Task.FromResult(principal);
         }
     }
 }
